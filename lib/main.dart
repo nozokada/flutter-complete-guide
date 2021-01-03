443555c5da9f2deaa159f24d1da909efdce024ba
Nozomi Okada <nozokada@gmail.com>
2021-01-02 22:51:12 -0700

    Added first widget tree

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,15 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Text('Hello!'));
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My First App'),
+        ),
+        body: Text('This is my default text!'),
+      ),
+    );
   }
 }
